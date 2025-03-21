673683eddf8d4296d1794e9a4fba65c73dfa7911
zihanpeter <luzihan1104@163.com>
2025-03-21 13:37:58 +0800

    2025-3-21

--- usaco2025febAs.cpp
+++ usaco2025febAs.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
+#include <stack>
 #include <algorithm>
 using namespace std;
 const int N = 2e5 + 5, INF = 0x3f3f3f3f;
-int n, a[N], cnt;
-struct node {
-    int x, pos;
-} b[N];
-bool cmp(node x, node y) {
-    if (x.x == y.x) return x.pos < y.pos;
-    return x.x > y.x;
-}
+int n, a[N], b[N], c[N], stkSeq[N], cnt;
+stack<int> stk;
 
 int main() {
-    // freopen("usaco2025febAs.in", "r", stdin);
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
@@ -22,18 +16,35 @@ int main() {
         cin >> n;
         for (int i = 1; i <= n; ++i) {
             cin >> a[i];
-            b[i].x = a[i];
-            b[i].pos = i;
         }
-        sort(b + 1, b + n + 1, cmp);
+        int maxi = n;
+        for (int i = n; i >= 1; --i) {
+            if (a[i] >= a[maxi]) maxi = i;
+            b[i] = maxi;
+        }
+        maxi = 1;
+        for (int i = 1; i <= n; ++i) {
+            c[i] = maxi;
+            if (a[i] >= a[maxi]) maxi = i;
+        }
+        for (int i = 1; i <= n; ++i) 
+            if (b[i] > i && a[c[b[i]]] <= a[b[i]]) {
+                int temp = a[b[i]];
+                for (int j = b[i]; j >= i; --j) a[j] = a[j - 1];
+                a[i] = temp;
+                break;
+            }
         cnt = 0;
         for (int i = 1; i <= n; ++i) {
-            if (b[i].pos < b[i - 1].pos) {
-                if (cnt) break;
-                cout << b[i].x << ' ';
-                cnt = 1;
-            } else cout << b[i].x << ' ';
+            while (!stk.empty() && stk.top() < a[i]) stk.pop();
+            stk.push(a[i]);
         }
+        while (!stk.empty()) {
+            stkSeq[++cnt] = stk.top();
+            stk.pop();
+        }
+        reverse(stkSeq + 1, stkSeq + cnt + 1);
+        for (int i = 1; i <= cnt; ++i) cout << stkSeq[i] << ' ';
         cout << '\n';
     }
     return 0;
